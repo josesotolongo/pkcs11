9a500296233116a2e9fe6028b83929260fc8524b
Jose Sotolongo <jys1021@dexcom.com>
2022-06-27 11:38:16 -0400

    Removed menu functons

--- loadDLL/main.cpp
+++ loadDLL/main.cpp
@@ -5,12 +5,6 @@
 
 using namespace std;
 
-#pragma region func declaration
-void DisplayMainMenu();
-void Options(int optionNum, crypt crypto);
-void startSession(crypt crypto);
-#pragma endregion
-
 int main(void)
 {
 	// Load the DLLs
@@ -25,55 +19,14 @@ int main(void)
 		return -1;
 	}
 	cout << "Cryptoki DLLs loaded successfully!" << endl;
+
+	crypto.LogAndDisplay(INFO, "Test");
+
+
 	crypto.InitializeCrypto();
+	crypto.GetMechInfo();
 	crypto.KeyCreation();
-
-	/*
-	bool exit = false;
-	while (!exit)
-	{
-		DisplayMainMenu();
-
-		int userInput;
-		cin >> userInput;
-
-		Options(userInput, crypto);
-
-		if (userInput == -1)
-		{
-			exit = true;
-		}
-	}
-	*/
 	crypto.FreeCrypto();
 
 	return 0;
 }
-
-#pragma region Functions
-void DisplayMainMenu()
-{
-	cout << "\nHSM Main Menu" << endl;
-	cout << "1. Cryptoki information." << endl;
-	cout << "2. Token information." << endl;
-	cout << "3. Session Menu." << endl;
-	cout << "\nEnter -1 to exit." << endl;
-}
-
-void Options(int optionNum, crypt crypto)
-{
-	switch (optionNum)
-	{
-		case 1:
-			crypto.DisplayInfo();
-			break;
-		case 2:
-			crypto.DisplayTokenInfo();
-			break;
-		case 3:
-			crypto.DisplaySessionMenu();
-		default:
-			break;
-	}
-}
-#pragma endregion
